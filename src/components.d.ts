f6f78d271fbd53da80eac9055a5098cf2a951e9b
WangSF <soullose@qq.com>
2024-11-24 22:11:18 +0800

    refactor: 移除未使用的 ICarbonAccessibility 组件导入
    - 从 GlobalComponents 接口中删除了 ICarbonAccessibility 的引用
    - 这个组件在项目中没有被使用，移除无效的导入可以提高代码的可维护性

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,7 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     AppFooter: typeof import('./components/AppFooter.vue')['default']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
-    ICarbonAccessibility: typeof import('~icons/carbon/accessibility')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
