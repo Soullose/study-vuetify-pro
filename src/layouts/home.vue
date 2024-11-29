7d06390f5e89cce7b18c100197f214f997df0430
WangSF <soullose@qq.com>
2024-11-29 21:01:14 +0800

    style(layouts): 优化 home 布局中的列表样式
    - 调整列表项的样式和间距
    - 添加自定义样式文件 test.scss
    - 修改 settings.scss，引入自定义样式和 Vuetify 设置

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -12,16 +12,22 @@
 
       <v-divider></v-divider>
 
-      <v-list density="compact" nav>
+      <v-list nav density="compact">
         <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-        <v-list-group value="Users">
+        <v-list-group dense link>
           <template v-slot:activator="{ props }">
             <v-list-item v-bind="props" prepend-icon="mdi-account-circle">
               <v-list-item-title class="font-weight-bold">测试</v-list-item-title>
             </v-list-item>
           </template>
-          <v-list-item prepend-icon="mdi-plus-outline" title="Home" value="Home" to="/"></v-list-item>
-          <v-list-item prepend-icon="mdi-file-outline" title="Test" value="Test" to="/test"</v-list-item>
+          <v-list-item class="d-flex align-center ml-0" link title="Home" value="Home" to="/">
+            <template v-slot:prepend>
+              <!-- <div class="d-flex align-center ml-4"> -->
+              <v-icon dense>mdi-plus-outline</v-icon>
+              <!-- </div> -->
+            </template>
+          </v-list-item>
+          <v-list-item prepend-icon="mdi-file-outline" title="Test" value="Test" to="/test"></v-list-item>
         </v-list-group>
         <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
         <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
@@ -85,3 +91,14 @@ function showRoutes() {
 
 const txxx = h('div', 'xxxxxxxxxxxxxxxxxx');
 </script>
+<style lang="scss" scoped>
+@use '@/styles/test';
+
+.v-list-group :deep(.v-list-item) {
+  padding-inline-start: test.$spacer !important;
+}
+
+.v-list-group--subgroup :deep(.v-list-group__items .v-list-item) {
+  padding-inline-start: test.$spacer * 5 !important;
+}
+</style>
