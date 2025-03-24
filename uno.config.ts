f3f064dad3a9a00b29f494a54d44f99b76d8ab14
WangSF <soullose@qq.com>
2025-03-24 14:55:59 +0800

    feat(uno.config): 添加并配置 Web 字体预设
    - 引入 preset-web-fonts 模块
    - 添加 Google 字体和其他自定义字体
    - 配置本地字体处理处理器
    - 设置字体缓存目录和资产保存位置

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,6 @@
 import presetIcons from '@unocss/preset-icons';
+import presetWebFonts from '@unocss/preset-web-fonts';
+import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local';
 import { defineConfig, presetUno } from 'unocss';
 export default defineConfig({
   presets: [
@@ -10,6 +12,38 @@ export default defineConfig({
         fa: () => import('@iconify/json/json/fa.json').then((i) => i.default)
         // logos: () => import('@iconify/json/json/logos.json').then((i) => i.default)
       }
+    }),
+    presetWebFonts({
+      /* options */
+      provider: 'google', // default provider
+      fonts: {
+        // these will extend the default theme
+        sans: 'Roboto',
+        mono: ['Fira Code', 'Fira Mono:400,700'],
+        // custom ones
+        lobster: 'Lobster',
+        lato: [
+          {
+            name: 'Lato',
+            weights: ['400', '700'],
+            italic: true
+          },
+          {
+            name: 'sans-serif',
+            provider: 'none'
+          }
+        ]
+      },
+      processors: createLocalFontProcessor({
+        // Directory to cache the fonts
+        cacheDir: 'node_modules/.cache/unocss/fonts',
+
+        // Directory to save the fonts assets
+        fontAssetsDir: 'public/assets/fonts',
+
+        // Base URL to serve the fonts from the client
+        fontServeBaseUrl: '/assets/fonts'
+      })
     })
   ]
 });
